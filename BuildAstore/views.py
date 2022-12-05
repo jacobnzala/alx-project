4ebc73286aa524b8f9ad786f522b0083f6e99e48
jacobnzala <nzalajacob@gmail.com>
2022-12-05 19:34:15 +0200

    new

--- BuildAstore/views.py
+++ BuildAstore/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from . models import Category, Product
+from .models import Category, Product
 
 def all_products(request):
     products = Product.objects.all()
